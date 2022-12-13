2be50adfda710d1de0cab0f845131b71e58570a0
abhi855 <abhiv762@gmail.com>
2022-12-13 23:08:06 +0530

    Hash Router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 import "./App.css";
-// import Cart from "./pages/Cart";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link,
-//   Redirect,
-// } from "react-router-dom";
-// import Gallery from "./pages/Gallery";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Gallery from "./pages/Gallery";
 
 function App() {
   const user = true;
   return (
-    <div>Hello</div>
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/">
-    //       <Home />
-    //     </Route>
-    //     <Route path="/products/:category">
-    //       <ProductList />
-    //     </Route>
-    //     <Route path="/product/:id">
-    //       <Product />
-    //     </Route>
-    //     <Route path="/gallery">
-    //       <Gallery />
-    //     </Route>
-    //     <Route path="/cart">
-    //       <Cart />
-    //     </Route>
-    //     <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
-    //     <Route path="/register">
-    //       {user ? <Redirect to="/" /> : <Register />}
-    //     </Route>
-    //   </Switch>
-    // </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/gallery">
+          <Gallery />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
