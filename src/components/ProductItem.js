b0b232cbf7dfa94bedd33bb7955350b9b9ca1c7d
vishalgusaiwal9352 <vishalgusaiwal66@gmail.com>
2023-01-17 01:01:52 +0530

    Product

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -9,11 +9,11 @@ import { imageLink, productsSecond } from "../data";
 import { mobile } from "../responsive";
 
 const Info = styled.div`
-  width: 95%;
   z-index: 3;
   display: flex;
   flex-direction: column;
   margin: 0px 10px;
+  align-items: center;
 `;
 const Container = styled.div`
   width: 250px;
@@ -24,54 +24,41 @@ const Container = styled.div`
   margin: 5px;
   align-items: center;
   justify-content: center;
-  z-index: 4;
-  /* display: flex;
-  flex-direction: column; */
-  /* transition: all 0.5s ease; */ /* &:hover ${Info} {
-    opacity: 1;
-  }
-  &:hover {
-    transform: scale(1.05);
-    z-index: 2;
-  } */ /* cursor: pointer; */
-  ${mobile({ width: "47vw", height: "250px" })};
+  ${mobile({ width: "46vw", height: "250px",boxShadow:"1px 1px 30px grey"})};
 `;
 const ImageContainer = styled.div`
   height: 75%;
   justify-content: center;
   display: flex;
-  /*padding: 2px;*/
   align-items: center;
+  width: 100%;
+
+  ${mobile({ width: "100%", })}
 `;
 const Image = styled.img`
-  /* padding: 2px 0px; */
   height: 100%;
-  /* width: 244px; */
-  margin: 3px;
   object-fit: cover;
-  ${mobile({ width: "45.5vw" })}
+  ${mobile({ width: "90%", })}
 `;
 
 const RsTag = styled.h3`
-  align-self: center;
   font-family: "Gudea", sans-serif;
   color: black;
 `;
 const Title = styled.h5`
   color: #23a1d1;
-  align-self: center;
   font-family: Verdana, sans-serif;
 `;
 const Company = styled.h6`
   color: #23a1d1;
-  padding: 2px;
-  align-self: center;
+  padding:2px 0px;
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   opacity: 0.8;
 `;
 const ProductItem = ({ item }) => {
   return (
     <Container>
+      
       <ImageContainer>
         <Image
           src={`http://43.205.207.87/${item.thumb}`}
